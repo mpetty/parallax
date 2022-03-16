764412859144c6266cc34de7205365013afdf740
Mitchell Petty <mpetty@users.noreply.github.com>
2022-03-15 20:08:38 -0400

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,35 @@
-/**
- * 	To use:
- *
- * 	npm install
- * 	bower install
- * 	gulp or gulp <task>
- */
 
-    const gulp = require('gulp');
+const gulp = require('gulp');
+const $ = require('gulp-load-plugins')({ camelize: true });
 
-    const $ = require('gulp-load-plugins')({ camelize: true });
+const paths = {
+    js_source : 'src/jquery.parallax.js',
+    js_dest : 'jquery.parallax.min.js'
+};
 
-    const paths = {
-        js_source : 'src/jquery.parallax.js',
-        js_dest : 'jquery.parallax.min.js'
-    };
+const uglifyOptions = {
+    compress: {
+        drop_console: true
+    },
+    output: {
+        comments: /^!|@preserve|@license|@cc_on/i
+    },
+};
 
-    const uglifyOptions = {
-        compress: {
-            drop_console: true
-        },
-        output: {
-            comments: /^!|@preserve|@license|@cc_on/i
-        },
-    };
+function jsTask(done) {
+    gulp.src(paths.js_source)
+        .pipe($.jshint())
+        .pipe($.concat({path: paths.js_dest}))
+        .pipe($.uglify(uglifyOptions))
+        .on('error', function(e) { console.error(e.toString()); })
+        .pipe(gulp.dest('.'));
 
-    function jsTask(done) {
-		gulp.src(paths.js_source)
-			.pipe($.jshint())
-            .pipe($.concat({path: paths.js_dest}))
-			.pipe($.uglify(uglifyOptions))
-            .on('error', function(e) { console.error(e.toString()); })
-			.pipe(gulp.dest('.'));
+    done();
+}
 
-        done();
-    }
+function watchTask(done) {
+    gulp.watch(paths.js_source, gulp.parallel(jsTask));
+};
 
-    function watchTask(done) {
-		gulp.watch(paths.js_source, gulp.parallel(jsTask));
-
-    };
-
-    exports.watch = watchTask;
-    exports.default = gulp.parallel(jsTask);
+exports.watch = watchTask;
+exports.default = gulp.parallel(jsTask);
